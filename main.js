2db609b8f0d872e608ea19df2ed31a5b64fb0f19
andreucc <andreucc@protonmail.com>
2019-02-13 14:43:41 +0100

    game logic done

--- main.js
+++ main.js
@@ -1,32 +1,54 @@
-'use strict';
-const main = () => {
-  console.log("hey");
+const main = () =>{
+    const buildDom = (html) => {
+        const main = document.querySelector("main");
+        main.innerHTML = html;
+        return main;
+    };
 
+    const buildSplashScreen = () => {
+        const splashScreen = buildDom(`
+        <section class = "splash-screen">
+        <h1>Eternal Enemies</h1>
+        <button>Start</button>
+        </section>
+            `)
+        const startButton = document.querySelector('button');
+        startButton.addEventListener('click', buildGameScreen);
+    };
 
-  const buildDom = (html) => {
-    const main = document.querySelector('main');
-    main.innerHTML = html;
-    return main;
-  }
+    const buildGameScreen = () => {
+        const gameScreen = buildDom(`
+        <section class = "game-screen">
+            <canvas></canvas>
+        </section>
+        `);
 
+        const width = document.querySelector(`.game-screen`).offsetWidth;
+        const height = document.querySelector(`.game-screen`).offsetHeight;
+    
+        const canvasElement = document.querySelector('canvas');
+    
+        canvasElement.setAttribute('width', width);
+        canvasElement.setAttribute('height', height);
+        
+        setTimeout(buildGameOver, 3000);
 
-  const buidSplashScreen = () => {
-    const splashScreen = buildDom(`
-    <section class="splas-screen">
-      <h1>hello</h1>  
-      <button>start</button>
-    </section>  
-      
-      
-      
-      
-      `)
-  };
+        const game = new Game(canvasElement);
+        game.startLoop();
+    }
 
+    const buildGameOver = () => {
+        const gameOverScreen = buildDom(`
+            <section class = "games-over">
+            <h1>Game Over Screen</h1>
+            <button>Restart</button>
+        `);
+        const restartButton = document.querySelector('button');
+        restartButton.addEventListener('click', buildGameScreen);
+    }
 
-
-buidSplashScreen();
-};
+    buildSplashScreen();
+}
 
 
 window.addEventListener('load', main);
