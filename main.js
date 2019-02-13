2d9087eb5abc869350ad0cf08353522e00f74e3f
andreucc <andreucc@protonmail.com>
2019-02-13 14:05:29 +0100

    tenemos boton

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
 'use strict';
+const main = () => {
+  console.log("hey");
+
+
+  const buildDom = (html) => {
+    const main = document.querySelector('main');
+    main.innerHTML = html;
+    return main;
+  }
+
+
+  const buidSplashScreen = () => {
+    const splashScreen = buildDom(`
+    <section class="splas-screen">
+      <h1>hello</h1>  
+      <button>start</button>
+    </section>  
+      
+      
+      
+      
+      `)
+  };
+
+
+
+buidSplashScreen();
+};
+
+
+window.addEventListener('load', main);
